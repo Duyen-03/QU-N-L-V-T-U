312c4d1232d49650b22fc081a46afa4bec705708
Duyen-03 <157661508+Duyen-03@users.noreply.github.com>
2025-04-13 11:33:21 +0700

    Add files via upload

---
+++ QUANLY_VETAU/vé tàu/frm_UPDATEVTAU.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace BTN_QLIVETAU
+{
+    partial class frm_UPDATEVTAU
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_GIAVE = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbo_SOTOA = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboGADEN = new System.Windows.Forms.ComboBox();
+            this.cboGADI = new System.Windows.Forms.ComboBox();
+            this.txt_TENTAU = new System.Windows.Forms.TextBox();
+            this.time_Den = new System.Windows.Forms.DateTimePicker();
+            this.time_Di = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_LUU = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txt_GIAVE
+            // 
+            this.txt_GIAVE.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txt_GIAVE.Location = new System.Drawing.Point(706, 269);
+            this.txt_GIAVE.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_GIAVE.Name = "txt_GIAVE";
+            this.txt_GIAVE.Size = new System.Drawing.Size(224, 26);
+            this.txt_GIAVE.TabIndex = 23;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(611, 273);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 20);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Giá vé";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(102, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Toa tàu";
+            // 
+            // cbo_SOTOA
+            // 
+            this.cbo_SOTOA.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cbo_SOTOA.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbo_SOTOA.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.cbo_SOTOA.FormattingEnabled = true;
+            this.cbo_SOTOA.Location = new System.Drawing.Point(181, 263);
+            this.cbo_SOTOA.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbo_SOTOA.Name = "cbo_SOTOA";
+            this.cbo_SOTOA.Size = new System.Drawing.Size(150, 28);
+            this.cbo_SOTOA.TabIndex = 20;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.cboGADEN);
+            this.groupBox1.Controls.Add(this.cboGADI);
+            this.groupBox1.Controls.Add(this.txt_TENTAU);
+            this.groupBox1.Controls.Add(this.time_Den);
+            this.groupBox1.Controls.Add(this.time_Di);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(79, 40);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(873, 215);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin Tàu";
+            // 
+            // cboGADEN
+            // 
+            this.cboGADEN.FormattingEnabled = true;
+            this.cboGADEN.Location = new System.Drawing.Point(151, 91);
+            this.cboGADEN.Name = "cboGADEN";
+            this.cboGADEN.Size = new System.Drawing.Size(224, 28);
+            this.cboGADEN.TabIndex = 18;
+            // 
+            // cboGADI
+            // 
+            this.cboGADI.FormattingEnabled = true;
+            this.cboGADI.Location = new System.Drawing.Point(151, 31);
+            this.cboGADI.Name = "cboGADI";
+            this.cboGADI.Size = new System.Drawing.Size(224, 28);
+            this.cboGADI.TabIndex = 17;
+            // 
+            // txt_TENTAU
+            // 
+            this.txt_TENTAU.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txt_TENTAU.Location = new System.Drawing.Point(151, 160);
+            this.txt_TENTAU.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_TENTAU.Name = "txt_TENTAU";
+            this.txt_TENTAU.Size = new System.Drawing.Size(224, 26);
+            this.txt_TENTAU.TabIndex = 16;
+            // 
+            // time_Den
+            // 
+            this.time_Den.CalendarMonthBackground = System.Drawing.SystemColors.MenuBar;
+            this.time_Den.CustomFormat = "dd/MM/yyyy";
+            this.time_Den.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.time_Den.Location = new System.Drawing.Point(558, 91);
+            this.time_Den.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.time_Den.Name = "time_Den";
+            this.time_Den.Size = new System.Drawing.Size(224, 26);
+            this.time_Den.TabIndex = 14;
+            // 
+            // time_Di
+            // 
+            this.time_Di.CalendarMonthBackground = System.Drawing.SystemColors.MenuBar;
+            this.time_Di.CustomFormat = "dd/MM/yyyy";
+            this.time_Di.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.time_Di.Location = new System.Drawing.Point(558, 34);
+            this.time_Di.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.time_Di.Name = "time_Di";
+            this.time_Di.Size = new System.Drawing.Size(224, 26);
+            this.time_Di.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(24, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Tên tàu";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(444, 91);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Giờ đến";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(24, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ga đi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(24, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ga đến";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(444, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Giờ đi";
+            // 
+            // btn_LUU
+            // 
+            this.btn_LUU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btn_LUU.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_LUU.Location = new System.Drawing.Point(809, 388);
+            this.btn_LUU.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_LUU.Name = "btn_LUU";
+            this.btn_LUU.Size = new System.Drawing.Size(143, 44);
+            this.btn_LUU.TabIndex = 256;
+            this.btn_LUU.Text = "Lưu";
+            this.btn_LUU.UseVisualStyleBackColor = false;
+            this.btn_LUU.Click += new System.EventHandler(this.btn_LUU_Click);
+            // 
+            // frm_UPDATEVTAU
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 793);
+            this.Controls.Add(this.btn_LUU);
+            this.Controls.Add(this.txt_GIAVE);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbo_SOTOA);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frm_UPDATEVTAU";
+            this.Text = "frm_UPDATEVTAU";
+            this.Load += new System.EventHandler(this.frm_UPDATEVTAU_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_GIAVE;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbo_SOTOA;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_TENTAU;
+        private System.Windows.Forms.DateTimePicker time_Den;
+        private System.Windows.Forms.DateTimePicker time_Di;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_LUU;
+        private System.Windows.Forms.ComboBox cboGADEN;
+        private System.Windows.Forms.ComboBox cboGADI;
+    }
+}
---
+++ QUANLY_VETAU/vé tàu/frm_UPDATEVTAU.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BTN_QLIVETAU
+{
+    public partial class frm_UPDATEVTAU : Form
+    {
+
+        string _maVe = "";
+        public frm_UPDATEVTAU()
+        {
+            InitializeComponent();
+        }
+        public frm_UPDATEVTAU(string maVe)  // Sửa
+        {
+            InitializeComponent();
+            _maVe = maVe;
+        }
+
+
+        KETNOI_DULIEU kn = new KETNOI_DULIEU();
+
+        
+
+        public void SOTOA()
+        {
+            DataTable dta = new DataTable();
+            dta = kn.Lay_DuLieuBang("Select * from TAU");
+            cbo_SOTOA.DataSource = dta;
+            cbo_SOTOA.DisplayMember = "SoToa";
+        }
+
+        public void GADI()
+        {
+            DataTable dta = new DataTable();
+            dta = kn.Lay_DuLieuBang("Select * from GATAU");
+            cboGADI.DataSource = dta;
+            cboGADI.DisplayMember = "TenGa";
+        }
+
+        public void GADEN()
+        {
+            DataTable dta = new DataTable();
+            dta = kn.Lay_DuLieuBang("Select * from GATAU");
+            cboGADEN.DataSource = dta;
+            cboGADEN.DisplayMember = "TenGa";
+        }
+
+        private void frm_UPDATEVTAU_Load(object sender, EventArgs e)
+        {
+            SOTOA();
+            GADI();
+            GADEN();
+
+            if (!string.IsNullOrEmpty(_maVe))
+            {
+                string sql = "SELECT * FROM V_TTVETAU WHERE MaVe = '" + _maVe + "'";
+                DataTable dta = kn.Lay_DuLieuBang(sql);
+
+                if (dta.Rows.Count > 0)
+                {
+                    txt_TENTAU.Text = dta.Rows[0]["TenTau"].ToString();
+                    txt_GIAVE.Text = dta.Rows[0]["GiaVe"].ToString();
+                    cboGADI.Text = dta.Rows[0]["GaDi"].ToString();
+                    cboGADEN.Text = dta.Rows[0]["GaDen"].ToString();
+                    time_Di.Text = dta.Rows[0]["GioDi"].ToString();
+                    time_Den.Text = dta.Rows[0]["GioDen"].ToString();
+                    cbo_SOTOA.Text = dta.Rows[0]["SoToa"].ToString();
+                }
+            }
+            //HIENTHI_DULIEU();
+        }
+
+        //public void HIENTHI_DULIEU()
+        //{
+        //    txt_TENTAU.DataBindings.Clear();
+        //    txt_TENTAU.DataBindings.Add("Text", dataGd_VE.DataSource, "TenTau");
+        //    txt_GIAVE.DataBindings.Clear();
+        //    txt_GIAVE.DataBindings.Add("Text", dataGd_VE.DataSource, "GiaVe");
+        //    cboGADI.DataBindings.Clear();
+        //    cboGADI.DataBindings.Add("Text", dataGd_VE.DataSource, "GaDi");
+        //    cboGADEN.DataBindings.Clear();
+        //    cboGADEN.DataBindings.Add("Text", dataGd_VE.DataSource, "GaDen");
+        //    time_Di.DataBindings.Clear();
+        //    time_Di.DataBindings.Add("Text", dataGd_VE.DataSource, "GioDi");
+        //    time_Den.DataBindings.Clear();
+        //    time_Den.DataBindings.Add("Text", dataGd_VE.DataSource, "GioDen");
+        //    cbo_SOTOA.DataBindings.Clear();
+        //    cbo_SOTOA.DataBindings.Add("Text", dataGd_VE.DataSource, "SoToa");
+        //}
+
+        private void btn_THEM_Click(object sender, EventArgs e)
+        {
+            cboGADI.Text = "";
+            cboGADEN.Text = "";
+            txt_TENTAU.Text = "";
+            time_Di.Text = "";
+            time_Den.Text = "";
+            cbo_SOTOA.Text = "";
+            MessageBox.Show("Thêm vé tàu thành công!", "Thông báo");
+        }
+
+        private void btn_SUA_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            //    // Lấy mã tàu
+            //    string sql_MaTau = "SELECT MaTau FROM TAU WHERE TenTau = N'" + txt_TENTAU.Text + "'";
+            //    string maTau = kn.LayGiaTri(sql_MaTau);
+
+            //    // Lấy mã lịch trình (đảm bảo định dạng giờ đúng)
+            //    string gioDi = time_Di.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            //    string sql_MaLichTrinh = "SELECT MaLichTrinh FROM LICHTRINH WHERE GaDi = N'" + cboGADI.Text + "' AND GioDi = '" + gioDi + "'";
+            //    string maLichTrinh = kn.LayGiaTri(sql_MaLichTrinh);
+
+            //    // Lấy mã chuyến đi
+            //    string sql_MaChuyenDi = "SELECT MaChuyenDi FROM CHUYENDI WHERE MaTau = '" + maTau + "' AND MaLichTrinh = '" + maLichTrinh + "'";
+            //    string maChuyenDi = kn.LayGiaTri(sql_MaChuyenDi);
+
+            //    // Kiểm tra giá trị rỗng
+            //    if (string.IsNullOrEmpty(maTau) || string.IsNullOrEmpty(maLichTrinh) || string.IsNullOrEmpty(maChuyenDi))
+            //    {
+            //        MessageBox.Show("Không tìm thấy thông tin phù hợp để sửa. Vui lòng kiểm tra lại dữ liệu nhập!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        return;
+            //    }
+
+            //    // Kiểm tra giá vé
+            //    if (!decimal.TryParse(txt_GIAVE.Text, out decimal giaVe))
+            //    {
+            //        MessageBox.Show("Giá vé không hợp lệ. Vui lòng nhập đúng số!", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //        return;
+            //    }
+
+            //    // UPDATE VETAU
+            //    string sql_sua_VETAU = "UPDATE VETAU SET GiaVe = " + giaVe + " WHERE MaChuyenDi = '" + maChuyenDi + "';";
+
+            //    // UPDATE TAU
+            //    string sql_sua_TAU = "UPDATE TAU SET SoToa = " + cbo_SOTOA.Text + " WHERE MaTau = '" + maTau + "';";
+
+            //    // UPDATE LICHTRINH
+            //    string gioDen = time_Den.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            //    string sql_sua_LICHTRINH = "UPDATE LICHTRINH SET GaDen = N'" + cboGADEN.Text + "', GioDen = '" + gioDen + "' WHERE MaLichTrinh = '" + maLichTrinh + "';";
+
+            //    // Gộp và thực thi
+            //    string sql_sua = sql_sua_VETAU + sql_sua_TAU + sql_sua_LICHTRINH;
+            //    kn.ThucThi(sql_sua);
+
+            //    MessageBox.Show("Cập nhật thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //    BANGVETAU();
+            //    HIENTHI_DULIEU();
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Đã xảy ra lỗi khi cập nhật dữ liệu: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //}
+        }
+
+        private void btn_LUU_Click(object sender, EventArgs e)
+        {
+            // Lấy thông tin
+            string tenTau = txt_TENTAU.Text;
+            string gaDi = cboGADI.Text;
+            string gaDen = cboGADEN.Text;
+            string gioDi = time_Di.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            string gioDen = time_Den.Value.ToString("yyyy-MM-dd HH:mm:ss");
+            string soToa = cbo_SOTOA.Text;
+            string giaVe = txt_GIAVE.Text;
+
+            // Lấy MaTau
+            string sql_MaTau = "SELECT MaTau FROM TAU WHERE TenTau = N'" + tenTau + "'";
+            string maTau = kn.LayGiaTri(sql_MaTau);
+
+            // Lấy MaGa
+            string sql_GaDi = "SELECT MaGa FROM GATAU WHERE TenGa = N'" + gaDi + "'";
+            string maGaDi = kn.LayGiaTri(sql_GaDi);
+            string sql_GaDen = "SELECT MaGa FROM GATAU WHERE TenGa = N'" + gaDen + "'";
+            string maGaDen = kn.LayGiaTri(sql_GaDen);
+
+            // Kiểm tra hoặc thêm LichTrinh
+            string sql_LichTrinh = "SELECT MaLichTrinh FROM LICHTRINH WHERE GaDi = '" + maGaDi + "' AND GaDen = '" + maGaDen + "' AND GioDi = '" + gioDi + "'";
+            string maLichTrinh = kn.LayGiaTri(sql_LichTrinh);
+
+            if (string.IsNullOrEmpty(maLichTrinh))
+            {
+                // Chưa có thì thêm mới
+                string sqlInsertLT = "INSERT INTO LICHTRINH (GaDi, GaDen, GioDi, GioDen) VALUES ('" + maGaDi + "', '" + maGaDen + "', '" + gioDi + "', '" + gioDen + "')";
+                kn.ThucThi(sqlInsertLT);
+                maLichTrinh = kn.LayGiaTri("SELECT MAX(MaLichTrinh) FROM LICHTRINH");
+            }
+
+            // Kiểm tra hoặc thêm ChuyenDi
+            string sql_ChuyenDi = "SELECT MaChuyenDi FROM CHUYENDI WHERE MaTau = '" + maTau + "' AND MaLichTrinh = '" + maLichTrinh + "'";
+            string maChuyenDi = kn.LayGiaTri(sql_ChuyenDi);
+
+            if (string.IsNullOrEmpty(maChuyenDi))
+            {
+                string sqlInsertCD = "INSERT INTO CHUYENDI (MaLichTrinh, MaTau, GiaCoBan) VALUES ('" + maLichTrinh + "', '" + maTau + "', " + giaVe + ")";
+                kn.ThucThi(sqlInsertCD);
+                maChuyenDi = kn.LayGiaTri("SELECT MAX(MaChuyenDi) FROM CHUYENDI");
+            }
+
+            if (string.IsNullOrEmpty(_maVe))  // THÊM
+            {
+                string sqlInsertVe = "INSERT INTO VETAU (MaChuyenDi, SoGhe, GiaVe, DaDat, KhachHang) VALUES ('" + maChuyenDi + "', 'A1', " + giaVe + ", 0, 'customer1')";
+                kn.ThucThi(sqlInsertVe);
+                MessageBox.Show("Đã thêm vé thành công!");
+            }
+            else  // SỬA
+            {
+                string sqlUpdate = "UPDATE VETAU SET MaChuyenDi = '" + maChuyenDi + "', GiaVe = " + giaVe + " WHERE MaVe = '" + _maVe + "'";
+                kn.ThucThi(sqlUpdate);
+                MessageBox.Show("Đã cập nhật vé thành công!");
+            }
+
+            this.Close(); // Đóng form sau khi lưu
+        }
+
+    }
+}
+
